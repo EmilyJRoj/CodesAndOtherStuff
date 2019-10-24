9525ea51370cbff81cb8902145734b0d31c990d5
turtleartgallery <100157892@lubbockisd.net>
2019-10-24 08:58:12 -0500

    concatenation and repetition. Now with Indexing, Slicing and Subtracting.

--- CodesAndOtherStuff/CodesAndStuff.py
+++ CodesAndOtherStuff/CodesAndStuff.py
@@ -38,6 +38,19 @@ print(name[-1])
 for i in range(len(name)):
     print(name[i])
 
+# Slicing and Dicing
+# slicing operator:
+# makes Substrings.
+
+print(name[0:3])
+print(name[:5])
+print(name[6:9])
+print(name[6:])
+
+for i in range(1, len(name)+1):
+    print(name[0:i])
+
+
 
 
 
