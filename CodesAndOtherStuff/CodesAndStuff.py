7b4bd92faec7d12c937511226bfd1701f3137954
turtleartgallery <100157892@lubbockisd.net>
2019-10-24 08:37:07 -0500

    concatenation and repitition.

---
+++ CodesAndOtherStuff/CodesAndStuff.py
@@ -0,0 +1,28 @@
+# strings
+# data that falls within " " marks
+
+# concatenation
+# put 2 or more strings together
+
+firstname = "Emily"
+lastname = "Rojas"
+
+print(Emily + " " + Rojas)
+
+print(Fullname)
+
+# repitition
+# repition operator:
+
+print("hip "*2 + "Hooray!")
+
+def rowYourBoat():
+    print("Row, "*3 + 'your boat')
+    print("Gently down the stream.")
+    print("Merrily, "*4)
+    print("life is but a dream.")
+
+rowYourBoat()
+
+
+
