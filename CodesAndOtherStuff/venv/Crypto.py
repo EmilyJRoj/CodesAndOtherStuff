33aeb3d86548d25c53bc6f486fbd2a13495c0c59
turtleartgallery <100157892@lubbockisd.net>
2019-10-31 08:33:29 -0500

    Finished both the Encryption and Decryption parts of the assignment. :) (HAPPY HALLOWEEN)

--- CodesAndOtherStuff/venv/Crypto.py
+++ CodesAndOtherStuff/venv/Crypto.py
@@ -62,6 +62,19 @@ print ("cipher: ") + encrypt(text,s)
 
 # write a ceaserDecrypter(cipherText, shift)
 
+Letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for key in range(len(letters)):
+    translated = ''
+    for symbol in message:
+        if symbol in letters:
+            num = letters.find(symbol)
+            num = num - key
+            if num < 0:
+                num = num + len(letters)
+            translated = translated + letters[num]
+        else:
+            translated = translated + symbol
 
 
 
